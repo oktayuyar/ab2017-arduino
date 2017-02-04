9f736db8ba4c45f417d1f62f4feffa9979c9defc
oktayuyar <oktyuyar@gmail.com>
2017-02-04 22:55:24 +0300

    led parlaklığı

---
+++ ledparlakligi.ino
@@ -0,0 +1,16 @@
+const int ledPin=9;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(ledPin, 255);   
+  delay(1000);                       
+  analogWrite(ledPin, 127);    
+  delay(1000); 
+  analogWrite(ledPin, 0);    
+  delay(1000);
+}
